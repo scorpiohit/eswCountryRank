3d01a679895bd75311c1001871b9163c15fc37f0
scorpiohit <mohit311088@yahoo.com>
2018-01-22 15:27:08 +0000

    Add files via upload

--- excel_reading.py
+++ excel_reading.py
@@ -16,10 +16,16 @@ def Opening_excel(FileName):
     
     for i in xls.sheet_names:
         Sheet[i]= pd.read_excel(xls, i)
-        if i == 'InternalData_2017':
+        if (i == 'InternalData_2017' or
+            i == 'CompleteMatrix'):
             Sheet[i].set_index(['Brand','Country Code'],  inplace=True)
-        else:
+            CountryData[i] = Sheet[i].to_dict(orient='index') 
+        elif (i == 'ExternalData_2017' or
+                i == 'BrandFlagExternal'):
             Sheet[i].set_index(['Country Code'],  inplace=True)
-        CountryData[i] = Sheet[i].to_dict(orient='index') 
+            CountryData[i] = Sheet[i].to_dict(orient='index') 
+        else :
+            Sheet[i].set_index(['Code'],  inplace=True)
+            CountryData[i] = Sheet[i].to_dict(orient='dict') 
     
     return Sheet,CountryData
